fef24e1fa5db267170cfd2da4494b9b3245ddf48
ivymroow <ivymroow@users.noreply.github.com>
2026-07-28 11:07:44 -0400

    fix: TCP-only trackers (HTTPS + WebSocket), no UDP

--- server/torrent.js
+++ server/torrent.js
@@ -17,14 +17,10 @@ const client = new WebTorrent({
 });
 
 const TRACKERS = [
-  'udp://tracker.opentrackr.org:1337/announce',
-  'udp://tracker.coppersurfer.tk:6969/announce',
-  'udp://tracker.leechers-paradise.org:6969/announce',
-  'udp://p4p.arenabg.com:1337/announce',
-  'udp://tracker.internetwarriors.net:1337/announce',
-  'udp://tracker.cyberia.is:6969/announce',
-  'udp://open.demonii.com:1337/announce',
   'https://opentracker.i2p.rocks:443/announce',
+  'http://tracker.bt4g.com:2095/announce',
+  'wss://tracker.webtorrent.dev',
+  'wss://tracker.openwebtorrent.com',
 ];
 
 function makeMagnet(hash, name) {
