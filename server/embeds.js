b340b51a6a8a0c5e4f04f2f7e50110cd1ae9290a
ivymroow <ivymroow@users.noreply.github.com>
2026-08-07 17:42:57 -0400

    fix back button hash, add VidSrc embed, WS logo

--- server/embeds.js
+++ server/embeds.js
@@ -1,13 +1,13 @@
-const axios = require('axios');
-
 async function getEmbeds(imdbId, tmdbId, season, episode) {
   const id = imdbId;
   const embeds = [];
 
   if (season && episode) {
     embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${episode}` });
+    embeds.push({ name: 'VidSrc', url: `https://vidsrc.net/embed/tv/${id}/${season}/${episode}` });
   } else {
     embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embed/${id}` });
+    embeds.push({ name: 'VidSrc', url: `https://vidsrc.net/embed/${id}` });
   }
 
   return embeds.map((e, i) => ({ provider: e.name, embedUrl: e.url, hash: 'embed-' + i, quality: 'HD', seeds: 999, peers: 0, size: '', fileIndex: 0 }));
