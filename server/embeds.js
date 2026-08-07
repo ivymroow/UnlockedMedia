a83790fadcabb95d2796a0f2ffa688154bc35bf2
ivymroow <ivymroow@users.noreply.github.com>
2026-08-07 18:51:36 -0400

    swap VidSrc for VidLink embed provider

--- server/embeds.js
+++ server/embeds.js
@@ -4,10 +4,10 @@ async function getEmbeds(imdbId, tmdbId, season, episode) {
 
   if (season && episode) {
     embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embedtv/${id}&s=${season}&e=${episode}` });
-    embeds.push({ name: 'VidSrc', url: `https://vidsrc.net/embed/tv/${id}/${season}/${episode}` });
+    embeds.push({ name: 'VidLink', url: `https://vidlink.pro/tv/${id}/${season}/${episode}` });
   } else {
     embeds.push({ name: '2Embed', url: `https://www.2embed.cc/embed/${id}` });
-    embeds.push({ name: 'VidSrc', url: `https://vidsrc.net/embed/${id}` });
+    embeds.push({ name: 'VidLink', url: `https://vidlink.pro/movie/${id}` });
   }
 
   return embeds.map((e, i) => ({ provider: e.name, embedUrl: e.url, hash: 'embed-' + i, quality: 'HD', seeds: 999, peers: 0, size: '', fileIndex: 0 }));
